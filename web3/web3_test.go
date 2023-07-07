f185f283948f51570c1194daa48743ee746e78f5
yuanjun <yuanjun@blockpi.io>
2023-07-07 10:09:03 +0800

    test

--- web3/web3_test.go
+++ web3/web3_test.go
@@ -10,14 +10,14 @@ import (
 )
 
 func TestEthLogs(t *testing.T) {
-	web3, _ := NewWeb3Client("https://polygon-mumbai-bor.publicnode.com")
+	web3, _ := NewWeb3Client("http://127.0.0.1：2052")
 
 	ctx := context.Background()
 	param := ethereum.FilterQuery{
 		FromBlock: big.NewInt(36617935),
 		ToBlock:   big.NewInt(36618707),
 		Addresses: []common.Address{common.HexToAddress("0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789")},
-		Topics:    [][]common.Hash{{common.HexToHash("0x49628fd1471006c1482da88028e9ce4dbb080b815c9b0344d39e5a8e6ec1419f")}},
+		Topics:    [][]common.Hash{{common.HexToHash("0x49628fd1471006c1482da88028e9ce4dbb080b815c9b0344d39e5a8e6ec1419f"), common.HexToHash("0xf9b56a7687db6b03d227b3fced3778af9206f0306c18f445e83eefe426e125fd")}},
 	}
 	logs, _ := web3.Cli().FilterLogs(ctx, param)
 	ret, _ := json.Marshal(logs)
