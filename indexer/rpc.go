b32b22eb35674888576de9e730457dd9c1a62aa0
yuanjun <yuanjun@blockpi.io>
2023-07-10 13:16:44 +0800

    eth_getLogs

--- indexer/rpc.go
+++ indexer/rpc.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"github.com/BlockPILabs/erc4337_user_operation_indexer/database"
 	"github.com/BlockPILabs/erc4337_user_operation_indexer/rpc"
+	"io"
+	"reflect"
 	"strings"
 )
 
@@ -37,52 +39,123 @@ func eth_getLogsByUserOperation(s Rpc, req *rpc.JsonRpcMessage) *rpc.JsonRpcMess
 	return resp
 }
 
+func parseArgumentArray(dec *json.Decoder, types []reflect.Type) ([]reflect.Value, error) {
+	args := make([]reflect.Value, 0, len(types))
+	for i := 0; dec.More(); i++ {
+		if i >= len(types) {
+			return args, fmt.Errorf("too many arguments, want at most %d", len(types))
+		}
+		argval := reflect.New(types[i])
+		if err := dec.Decode(argval.Interface()); err != nil {
+			return args, fmt.Errorf("invalid argument %d: %v", i, err)
+		}
+		if argval.IsNil() && types[i].Kind() != reflect.Ptr {
+			return args, fmt.Errorf("missing value for required argument %d", i)
+		}
+		args = append(args, argval.Elem())
+	}
+	// Read end of args array.
+	_, err := dec.Token()
+	return args, err
+}
+
+func parsePositionalArguments(rawArgs json.RawMessage, types []reflect.Type) ([]reflect.Value, error) {
+	dec := json.NewDecoder(bytes.NewReader(rawArgs))
+	var args []reflect.Value
+	tok, err := dec.Token()
+	switch {
+	case err == io.EOF || tok == nil && err == nil:
+		// "params" is optional and may be empty. Also allow "params":null even though it's
+		// not in the spec because our own client used to send it.
+	case err != nil:
+		return nil, err
+	case tok == json.Delim('['):
+		// Read argument array.
+		if args, err = parseArgumentArray(dec, types); err != nil {
+			return nil, err
+		}
+	default:
+		return nil, fmt.Errorf("non-array args")
+	}
+	// Set any missing args to nil.
+	for i := len(args); i < len(types); i++ {
+		if types[i].Kind() != reflect.Ptr {
+			return nil, fmt.Errorf("missing value for required argument %d", i)
+		}
+		args = append(args, reflect.Zero(types[i]))
+	}
+	return args, nil
+
+}
+
+func isArray(raw []byte) bool {
+	for _, c := range raw {
+		// skip insignificant whitespace (http://www.ietf.org/rfc/rfc4627.txt)
+		if c == 0x20 || c == 0x09 || c == 0x0a || c == 0x0d {
+			continue
+		}
+		return c == '['
+	}
+	return false
+}
+
 func eth_getLogs(s Rpc, req *rpc.JsonRpcMessage) *rpc.JsonRpcMessage {
-	var params []map[string]any
+	var params []struct {
+		Address   json.RawMessage
+		Topics    []json.RawMessage
+		FromBlock string
+		ToBlock   string
+	}
+
 	err := json.Unmarshal(req.Params, &params)
 	if err != nil {
 		return rpc.NewJsonRpcMessageWithError(req.ID, -32000, "invalid json")
 	}
-
 	if len(params) != 1 {
 		return rpc.NewJsonRpcMessageWithError(req.ID, -32602, "too many arguments, want at most 1")
 	}
 
 	address := ""
-	v, ok := params[0]["address"]
-	if !ok {
-		return rpc.NewJsonRpcMessageWithError(req.ID, -32602, "address wanted")
-	}
-	switch v.(type) {
-	case string:
-		address = v.(string)
-	case []interface{}:
-		arr := v.([]interface{})
-		if len(arr) != 1 {
-			return rpc.NewJsonRpcMessageWithError(req.ID, -32000, "most 1 address")
+	param := params[0]
+	if isArray(param.Address) {
+		var addresses []string
+		json.Unmarshal(param.Address, &addresses)
+		if len(addresses) > 1 {
+			return rpc.NewJsonRpcMessageWithError(req.ID, -32602, "too many addresses, want at most 1")
 		}
-		address = fmt.Sprintf("%v", arr[0])
-	}
-	address = strings.ToLower(address)
-	if address != s.EntryPoint() {
-		return rpc.NewJsonRpcMessageWithError(req.ID, -32000, "address wanted")
+		if len(addresses) == 1 {
+			address = addresses[0]
+		}
+	} else {
+		json.Unmarshal(param.Address, &address)
 	}
 
-	v, ok = params[0]["topics"]
-	if !ok {
+	if len(address) == 0 {
+		return rpc.NewJsonRpcMessageWithError(req.ID, -32602, "address wanted")
+	}
+
+	address = strings.ToLower(address)
+	entrypoint := s.EntryPoint()
+	if address != entrypoint {
+		return rpc.NewJsonRpcMessageWithError(req.ID, -32000, "address mismatch entrypoint "+entrypoint)
+	}
+
+	if len(param.Topics) == 0 {
 		return rpc.NewJsonRpcMessageWithError(req.ID, -32000, "topics wanted")
 	}
 
-	var topics []any
-	switch v.(type) {
-	case []any:
-		topics = v.([]any)
-	case [][]any:
-		arr := v.([][]any)
-		if len(arr) != 1 {
-			return rpc.NewJsonRpcMessageWithError(req.ID, -32000, "most 1 group topics")
+	var topics []string
+	if isArray(param.Topics[0]) {
+		if len(param.Topics) > 1 {
+			return rpc.NewJsonRpcMessageWithError(req.ID, -32000, "too many topics, want at most 1")
 		}
-		topics = arr[0]
+		json.Unmarshal(param.Topics[0], &topics)
+	} else {
+		for _, topic := range param.Topics {
+			var topicStr string
+			json.Unmarshal(topic, &topicStr)
+			topics = append(topics, topicStr)
+		}
 	}
 
 	if len(topics) < 2 {
@@ -96,6 +169,21 @@ func eth_getLogs(s Rpc, req *rpc.JsonRpcMessage) *rpc.JsonRpcMessage {
 
 	opHash := fmt.Sprintf("%v", topics[1])
 	data, _ := s.Db().Get(DbKeyUserOp(opHash))
+
+	if len(data) > 0 {
+		info := struct {
+			Topics      []string
+			BlockNumber string
+		}{}
+		json.Unmarshal(data, &info)
+
+		for i := 0; i < len(topics) && i < len(info.Topics); i++ {
+			if topics[i] != info.Topics[i] {
+				data = []byte("")
+				break
+			}
+		}
+	}
 
 	result := bytes.Join([][]byte{[]byte("["), data, []byte("]")}, []byte(""))
 
